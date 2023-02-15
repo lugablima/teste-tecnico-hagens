08261e2b485e8bc88e5096a21b7752027b94af96
Lucas <lugablima@gmail.com>
2023-02-15 12:05:18 -0300

    chore: add scripts in back-end package.json file

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,5 +1,5 @@
 import app from "./app";
 
-const PORT = Number(process.env.PORT) | 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
