7430768017584fe407eda174f4d7baa687a83c04
Lucas <lugablima@gmail.com>
2023-02-16 15:43:15 -0300

    feat: add userRouter in index.ts file

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "./authRouter";
+import userRouter from "./userRouter";
 
 const router = Router();
 
 router.use(authRouter);
+router.use(userRouter);
 
 export default router;
