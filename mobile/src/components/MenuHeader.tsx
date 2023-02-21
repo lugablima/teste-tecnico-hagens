bf5c30dfdcd14ff4a4ea550031f1d795de2806b5
Lucas <lugablima@gmail.com>
2023-02-21 17:38:50 -0300

    feat: add username in menu header

--- mobile/src/components/MenuHeader.tsx
+++ mobile/src/components/MenuHeader.tsx
@@ -1,10 +1,13 @@
 import { Image, Text, View } from "react-native";
+import { UserContext, useUserContext } from "../contexts/UserContext";
 
 export function MenuHeader() {
+  const { user } = useUserContext() as UserContext;
+
   return (
     <View className="w-full bg-transparent items-start gap-y-2 border-b border-b-black py-9 px-5">
       <Image source={require("../assets/woman.png")} className="w-[60] h-[60] rounded-[250] object-cover object-center" />
-      <Text className="font-semibold text-xl text-black">Emily Smith</Text>
+      <Text className="font-semibold text-xl text-black">{user?.name}</Text>
     </View>
   );
 }
