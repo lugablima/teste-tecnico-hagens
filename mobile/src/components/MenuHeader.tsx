01aa4107f2f8788ecfe6fc29a5139718c2b9f667
Lucas <lugablima@gmail.com>
2023-02-22 17:05:28 -0300

    feat: add profile picture display in menu and edit screen

--- mobile/src/components/MenuHeader.tsx
+++ mobile/src/components/MenuHeader.tsx
@@ -1,4 +1,6 @@
 import { Image, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
 import { UserContext, useUserContext } from "../contexts/UserContext";
 
 export function MenuHeader() {
@@ -6,7 +8,7 @@ export function MenuHeader() {
 
   return (
     <View className="w-full bg-transparent items-start gap-y-2 border-b border-b-black py-9 px-5">
-      <Image source={require("../assets/woman.png")} className="w-[60] h-[60] rounded-[250] object-cover object-center" />
+      { user?.image ? <Image source={{ uri: `data:${user.image.type};base64,${user.image.data}` }} className="w-[60] h-[60] rounded-full object-cover object-center" /> : <Ionicons name="md-person-circle" size={60} />}
       <Text className="font-semibold text-xl text-black">{user?.name}</Text>
     </View>
   );
--- mobile/src/components/ProfileHeader.tsx
+++ mobile/src/components/ProfileHeader.tsx
@@ -1,15 +1,37 @@
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { useRoute } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
+import { UserContext, useUserContext } from "../contexts/UserContext";
+import { CameraContext, useCameraContext } from "../contexts/CameraContext";
 
 interface ProfileHeaderProps {
     header: string;
 }
 
-export function ProfileHeader({ header }: ProfileHeaderProps) {
+export function ProfileHeader({ header }: ProfileHeaderProps) { 
+    const route = useRoute();
+    const { user } = useUserContext() as UserContext; 
+    const { setOpenCamera } = useCameraContext() as CameraContext; 
+    
+    function RenderProfileImage() {
+        if(route.name === "Home") {
+            if(!user?.image) {
+                return <Ionicons name="md-person-circle" size={150} />;
+            }
+            return <Image source={{ uri: `data:${user.image.type};base64,${user.image.data}`}} className="w-[150] h-[150] rounded-full object-cover object-center" />;
+        }
+
+        if(!user?.image) return (<TouchableOpacity activeOpacity={0.7} onPress={() => setOpenCamera(true)} ><Ionicons name="md-person-circle" size={150} /></TouchableOpacity>);
+
+        return (<TouchableOpacity activeOpacity={0.7} onPress={() => setOpenCamera(true)}><Image source={{ uri: `data:${user.image.type};base64,${user.image.data}`}} className="w-[150] h-[150] rounded-full object-cover object-center" /></TouchableOpacity>);
+    }
+
+    const profileImage = RenderProfileImage();
+
     return(
-        <View className="items-center gap-y-6 mt-6 mb-6">
-            <Text className="font-bold text-3xl text-black">{ header }</Text>
-            <Ionicons name="md-person-circle" size={150} />
-        </View>
+            <View className="items-center gap-y-6 mt-6 mb-6">
+                <Text className="font-bold text-3xl text-black">{ header }</Text>
+                { profileImage }
+            </View>
     );
 }
